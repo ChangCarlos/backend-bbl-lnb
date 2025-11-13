feeba721af32cf779c6a2b14acb29f2bedb57ab9
ChangCarlos <carloshchang93@gmail.com>
2025-11-13 10:01:07 -0300

    ajuste no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { PrismaModule } from './modules/prisma/prisma.module';
-import { PrismaService } from './modules/prisma/prisma.service';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
@@ -10,13 +10,18 @@ import { UsersModule } from './modules/users/users.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [`.env.${process.env.NODE_ENV}`],
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
     }),
     PrismaModule,
     AuthModule,
-    UsersModule
+    UsersModule,
   ],
   controllers: [],
-  providers: [PrismaService, { provide: 'APP_MODULE', useClass: JwtAuthGuard }],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
